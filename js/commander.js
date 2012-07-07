ebbb6d021fd24e9a3128ab2009ce4e7aed2d38b0
greatghoul <greatghoul@gmail.com>
2012-07-07 22:23:13 +0800

    update event

--- js/commander.js
+++ js/commander.js
@@ -1,10 +1,13 @@
 var Commander = function() {
-    this.port = chrome.extension.connect({name: "commander"});
+    // 用于发送COMMAND命令的PORT  EXTENSION --> CONTENTSCRIPT
+
+    // 接受来自CONTENTSCRIPT的信息
     chrome.extension.onConnect.addListener(function(port) {
-        console.assert(port.name == "commander");
+        console.assert(port.name == 'MUSIC1G_STATUS');
         port.onMessage.addListener(function(msg) {
-            var evt = new CustomEvent(msg.type, {"detail": msg.body });
-            console.log(msg);
+            // 将接收到的状态信息模拟成事件，发送给对应的状态监听
+            console.log('>>> ACCEPT STATUS: ' + msg.type + ', ' + JSON.stringify(msg.body));
+            var evt = new CustomEvent(msg.type, { 'detail': msg.body });
             document.dispatchEvent(evt); 
         });
     });
@@ -14,11 +17,16 @@ var Commander = function() {
 Commander.prototype = {
     constructor: Commander,
 
-    bind: function(event, callback) {
-        document.addEventListener(event, function(evt) {
-            console.log(evt);
-            console.log('listener: ' + JSON.stringify(evt.detail));
+    bind: function(eventName, callback) {
+        document.addEventListener(eventName, function(evt) {
             callback(evt.detail); 
         }); 
+    },
+
+    command: function(type, option) {
+        if (!this.port) {
+            this.port = chrome.extension.connect({ name: 'MUSIC1G_COMMAND' });
+        }
+        this.port.postMessage({ 'type': type, 'option': option });
     }
 }
--- js/contentscript.js
+++ js/contentscript.js
@@ -1,18 +1,20 @@
-var port = chrome.extension.connect({name: 'commander'});
+// 建立向扩展页面发送播放状态信息的通道
+var port = chrome.extension.connect({name: 'MUSIC1G_STATUS'});
 
-document.addEventListener('music1g', function(evt) {
-    var data = evt.detail;
-    console.log(JSON.stringify(data));
-    port.postMessage(data);
+// 捕获甴COMMANDER代理页面传递回来的播放状态
+// 并通过 MUSIC1G_STATUS通道发送给扩展页面
+document.addEventListener('MUSIC1G_STATUS', function(evt) {
+    port.postMessage(evt.detail);
 });
 
-chrome.extension.onConnect.addListener(function(port){
-    console.assert(port.name == "commander");
-    port.onMessage.addListener(function(event){
-        if (event.type) {
-            var detail = { "type": type, "body": body };
-            var evt = new CustomEvent("music1g", { "detail": detail });
-            commandbox.dispatchEvent(evt);
-        }
+// 接受来自扩展页面发出的播放指令
+chrome.extension.onConnect.addListener(function(port) {
+    console.assert(port.name == 'MUSIC1G_COMMAND');
+    port.onMessage.addListener(function(msg) {
+        console.log('SEND COMMAND: type=' + msg.type 
+            + (msg.option ? ', option=' + JSON.stringify(msg.option) : ''));
+        var detail = { 'type': msg.type, 'option': msg.option };
+        var evt = new CustomEvent('MUSIC1G_COMMAND', { 'detail': detail });
+        document.dispatchEvent(evt);
     });
 });
